8e2bd6768e7c88c5258a2b74a82411f1c934d247
takahiroyoshikawa <aihasayahukoma7@gmail.com>
2019-03-19 00:03:05 +0900

    first commit

---
+++ SlideMenu/SlideMenuTests/SlideMenuTests.swift
@@ -0,0 +1,34 @@
+//
+//  SlideMenuTests.swift
+//  SlideMenuTests
+//
+//  Created by 吉川昂広 on 2019/03/18.
+//  Copyright © 2019 takahiro yoshikawa. All rights reserved.
+//
+
+import XCTest
+@testable import SlideMenu
+
+class SlideMenuTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
---
+++ TwoTabPage/TwoTabPage/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TwoTabPage
+//
+//  Created by 吉川昂広 on 2019/03/19.
+//  Copyright © 2019 takahiro yoshikawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
